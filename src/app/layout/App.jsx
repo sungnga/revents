54d03dbcab5902f9f1c0ef1c07f50e74a57377bd
sungnga <nga422@gmail.com>
2021-08-29 22:57:10 -0700

    Styling the HomePage

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -23,13 +23,20 @@ function App() {
 
 	return (
 		<>
-			<NavBar setFormOpen={handleCreateFormOpen} />
-			<Container className='main'>
-				<Route exact path='/' component={HomePage} />
-				<Route exact path='/events' component={EventDashboard} />
-				<Route path='/events/:id' component={EventDetailedPage} />
-				<Route path='/createEvent' component={EventForm} />
-			</Container>
+			<Route exact path='/' component={HomePage} />
+			<Route
+				path={'/(.+)'}
+				render={() => (
+					<>
+						<NavBar setFormOpen={handleCreateFormOpen} />
+						<Container className='main'>
+							<Route exact path='/events' component={EventDashboard} />
+							<Route path='/events/:id' component={EventDetailedPage} />
+							<Route path='/createEvent' component={EventForm} />
+						</Container>
+					</>
+				)}
+			/>
 		</>
 	);
 }
