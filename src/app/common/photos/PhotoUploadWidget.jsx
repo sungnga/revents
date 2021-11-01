d99dc69abb83f63976bed5a1e6304647fef14557
sungnga <nga422@gmail.com>
2021-11-01 12:36:57 -0700

    React-cropper library: PhotoWidgetCropper

--- src/app/common/photos/PhotoUploadWidget.jsx
+++ src/app/common/photos/PhotoUploadWidget.jsx
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
-import { Grid, Header } from 'semantic-ui-react';
+import { Button, Grid, Header } from 'semantic-ui-react';
+import PhotoWidgetCropper from './PhotoWidgetCropper';
 import PhotoWidgetDropzone from './PhotoWidgetDropzone';
 
 function PhotoUploadWidget() {
 	const [files, setFiles] = useState([]);
+	const [image, setImage] = useState(null);
 
 	return (
 		<Grid>
@@ -14,10 +16,30 @@ function PhotoUploadWidget() {
 			<Grid.Column width={1} />
 			<Grid.Column width={4}>
 				<Header color='teal' sub content='Step 2 - Resize' />
+				{/* first check to make sure we have a file before displaying PhotoWidgetCropper */}
+				{/* the preview property contains the image file that we get from dropzone */}
+				{files.length > 0 && (
+					<PhotoWidgetCropper
+						setImage={setImage}
+						imagePreview={files[0].preview}
+					/>
+				)}
 			</Grid.Column>
 			<Grid.Column width={1} />
 			<Grid.Column width={4}>
 				<Header color='teal' sub content='Step 3 - Preview & Upload' />
+				{files.length > 0 && (
+					<>
+						<div
+							className='img-preview'
+							style={{ minHeight: 200, minWidth: 200, overflow: 'hidden' }}
+						/>
+						<Button.Group>
+							<Button style={{ width: 100 }} positive icon='check' />
+							<Button style={{ width: 100 }} icon='close' />
+						</Button.Group>
+					</>
+				)}
 			</Grid.Column>
 		</Grid>
 	);
---
+++ src/app/common/photos/PhotoWidgetCropper.jsx
@@ -0,0 +1,38 @@
+import React, { useRef } from 'react';
+import Cropper from 'react-cropper';
+import 'cropperjs/dist/cropper.css';
+
+function PhotoWidgetCropper({ setImage, imagePreview }) {
+	const cropperRef = useRef(null);
+
+	function cropImage() {
+		const imageElement = cropperRef?.current;
+		const cropper = imageElement?.cropper;
+
+		if (typeof cropper.getCroppedCanvas() === 'undefined') {
+			return;
+		}
+		cropper.getCroppedCanvas().toBlob((blob) => {
+			setImage(blob);
+		}, 'image/jpeg');
+	}
+
+	return (
+		<Cropper
+			ref={cropperRef}
+			src={imagePreview}
+			style={{ height: 200, width: '100%' }}
+			// Cropper.js options
+			initialAspectRatio={1}
+			preview='.img-preview'
+			guides={false}
+			viewMode={1}
+			dragMode='move'
+			scalable={true}
+			cropBoxMovable={true}
+			cropBoxResizable={true}
+			crop={cropImage}
+		/>
+	);
+}
+export default PhotoWidgetCropper;
