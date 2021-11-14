7bca46c71611908f48ad5042f0ad843f38435842
sungnga <nga422@gmail.com>
2021-11-13 21:38:59 -0800

    Displaying the replies

--- src/app/common/util/utils.js
+++ src/app/common/util/utils.js
@@ -5,3 +5,16 @@ export function delay(ms) {
 export function getFileExtension(filename) {
 	return filename.slice(((filename.lastIndexOf('.') - 1) >>> 0) + 2);
 }
+
+// create a data tree of comments array (dataset)
+// the dataset we get from firebase realtime db
+export function createDataTree(dataset) {
+	let hashtable = Object.create(null);
+	dataset.forEach((a) => (hashtable[a.id] = { ...a, childNodes: [] }));
+	let dataTree = [];
+	dataset.forEach((a) => {
+		if (a.parentId) hashtable[a.parentId].childNodes.push(hashtable[a.id]);
+		else dataTree.push(hashtable[a.id]);
+	});
+	return dataTree;
+}
